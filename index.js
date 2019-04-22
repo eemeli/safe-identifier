567f3a236f399ea5f81a68c60844bb856d509e56
Eemeli Aro <eemeli@gmail.com>
2019-04-22 15:46:57 +0300

    Refactor propname() as property(obj, key)

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const { reservedES3, reservedES5 } = require('./reserved')
 
 /**
- * Utility function for quoting an Object's key value if required
+ * Sanitize a string for use as a property name
  *
- * Quotes the key if it contains invalid characters or is an
- * ECMAScript 3rd Edition reserved word (for IE8).
+ * By default uses `obj.key` notation, falling back to `obj["key"]` if the key
+ * contains invalid characters or is an ECMAScript 3rd Edition reserved word
+ * (required by IE8).
+ *
+ * @param {string} [obj] If empty, returns only the possibly quoted key
+ * @param {string} key The property name
+ * @returns {string}
  */
-function propname(key, obj) {
+function property(obj, key) {
   if (/^[A-Z_$][0-9A-Z_$]*$/i.test(key) && !reservedES3[key]) {
-    return obj ? `${obj}.${key}` : key;
+    return obj ? obj + '.' + key : key
   } else {
-    const jkey = JSON.stringify(key);
-    return obj ? obj + `[${jkey}]` : jkey;
+    const jkey = JSON.stringify(key)
+    return obj ? obj + '[' + jkey + ']' : jkey
   }
 }
 
@@ -23,4 +28,4 @@ function funcname(key) {
   return reservedES3[fn] || reservedES5[fn] || /^\d/.test(fn) ? '_' + fn : fn;
 }
 
-module.exports = { funcname, propname }
+module.exports = { funcname, property }

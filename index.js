b54921e0bfd9aa6c35f2933cb6dde05e2e1edc7a
Eemeli Aro <eemeli@gmail.com>
2019-04-22 15:42:04 +0300

    Refactor exports as CommonJS

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const { reservedES3, reservedES5 } = require('./reserved')
  *
  * Quotes the key if it contains invalid characters or is an
  * ECMAScript 3rd Edition reserved word (for IE8).
- *
- * @private
  */
-export function propname(key, obj) {
+function propname(key, obj) {
   if (/^[A-Z_$][0-9A-Z_$]*$/i.test(key) && !reservedES3[key]) {
     return obj ? `${obj}.${key}` : key;
   } else {
@@ -19,10 +17,10 @@ export function propname(key, obj) {
 
 /**
  * Utility function for escaping a function name if required
- *
- * @private
  */
-export function funcname(key) {
+function funcname(key) {
   const fn = key.trim().replace(/\W+/g, '_');
   return reservedES3[fn] || reservedES5[fn] || /^\d/.test(fn) ? '_' + fn : fn;
 }
+
+module.exports = { funcname, propname }
